a00928f6da7e0aa8f1271e518e705fa9f5fac571
Pandiyan-Mattew <pansmagimai@gmail.com>
2016-12-21 18:01:21 +0530

    Add files via upload

---
+++ isotope/isotope.custom.js
@@ -0,0 +1,11 @@
+jQuery( document ).ready( function( $ ) {
+	var $container = $('#isotope-container');
+	    $container.isotope({
+	    itemSelector : '.isotope-item',
+	});
+	$('#filters a').click(function(){
+	    var selector = $(this).attr('data-filter');
+	    $container.isotope({ filter: selector });
+	    return false;
+	});
+});
